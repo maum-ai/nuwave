c82ce71338065f00d631cb18e6c2f54c2121c410
승우 한 <hansw0326@mindslab.ai>
2021-07-05 14:11:50 +0900

    singlespk

--- dataloader.py
+++ dataloader.py
@@ -119,3 +119,68 @@ class VCTKMultiSpkDataset(Dataset):
         #wav_l = rosa.resample(wav, hparams.audio.sr, hparams.audio.sr//hparams.audio.ratio)
         wav_l = self.upsample(wav_l).view(1,-1)
         return wav, wav_l
+
+
+class VCTKSingleSpkDataset(Dataset):
+    def __init__(self, hparams, cv=0):  # cv 0: train, 1: val, 2: test
+        def _get_datalist(folder, file_format, cv):
+            _dl = []
+            audio_list = sorted(glob(path.join(folder, file_format)))
+            len_audio_list = len(audio_list)
+            s=0
+            print(f'full audios {len_audio_list}')
+            for i, audio in enumerate(audio_list):
+                if cv == 0:
+                    if not (i < int(len_audio_list * self.cv_ratio[0])): continue
+                elif cv == 1:
+                    if not (int(len_audio_list * self.cv_ratio[0]) <= i and
+                            i <= int(len_audio_list * (self.cv_ratio[0] + self.cv_ratio[1]))):
+                        continue
+                else:
+                    if not (int(len_audio_list * self.cv_ratio[0]) <= i and
+                            i <= int(len_audio_list * (self.cv_ratio[0] + self.cv_ratio[1]))):
+                        continue
+                s+=1
+                _dl.append(audio)
+
+            print(cv, s)
+            return _dl
+
+        self.hparams = hparams
+        self.cv = cv
+        self.cv_ratio = eval(hparams.data.cv_ratio)
+        self.directory = hparams.data.dir
+        self.dataformat = hparams.data.format
+        self.data_list = _get_datalist(self.directory, self.dataformat, self.cv)
+
+        self.filter_ratio = [1./hparams.audio.ratio]
+        self.lowpass = LowPass(hparams.audio.nfft,
+                               hparams.audio.hop,
+                               ratio=self.filter_ratio)
+        self.upsample = torch.nn.Upsample(scale_factor=hparams.audio.ratio,
+                                          mode ='linear',
+                                          align_corners = False)
+        assert len(self.data_list) != 0, "no data found"
+
+    def __len__(self):
+        return len(self.data_list)
+
+    def __getitem__(self, index):
+        wav = torch.load(self.data_list[index])
+        wav /= wav.abs().max()
+        if wav.shape[0] < self.hparams.audio.length:
+            padl = self.hparams.audio.length - wav.shape[0]
+            r = random.randint(0, padl) if self.cv<2 else padl//2
+            wav = torch.nn.functional.pad(wav, (r, padl-r), 'constant', 0)
+        else:
+            start = random.randint(0, wav.shape[0] - self.hparams.audio.length)
+            wav = wav[start:start+self.hparams.audio.length] if self.cv<2 \
+                    else wav[:len(wav)-len(wav)%self.hparams.audio.ratio]
+        wav *= random.random()/2+0.5 if self.cv<2 else 1
+
+        wav_l = self.lowpass(wav, 0)
+        wav_l = wav_l[0,::self.hparams.audio.ratio].view(1,1,-1)
+        #or
+        #wav_l = rosa.resample(wav, hparams.audio.sr, hparams.audio.sr//hparams.audio.ratio)
+        wav_l = self.upsample(wav_l).view(1,-1)
+        return wav, wav_l
